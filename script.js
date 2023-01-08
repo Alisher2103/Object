aa6449a9d794473e49f55badfee097a2ed6db20c
Alisher2103 <alishe2103edward@gmail.com>
2023-01-09 02:29:03 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// 1
+
+// let user = {}
+// let user = {
+//     name: 'John'
+// };
+
+//
+
+// let user = {
+//     name: 'John'
+// };
+
+// user.surname = 'Smith'
+// user.name = 'Pete'
+// delete user.name
+// console.log(user);
