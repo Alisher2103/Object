c745c37250a8680ba0163cd3568337b89fee62d4
Alisher2103 <99031506+Alisher2103@users.noreply.github.com>
2023-01-09 18:04:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,47 @@
 // user.name = 'Pete'
 // delete user.name
 // console.log(user)`
+// 2
+// let schedule = {};
+// schedule = new Object() && console.log('True');
+
+// let schedule = {
+//     '8:30' : 'get up'
+// }
+
+// console.log(schedule['8:30']);
+// console.log(schedule == new Object);
+
+// // 3
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete : 130
+// }
+
+// let total = 0;
+
+// for (let value in salaries) {
+//     total += salaries[value];
+// }
+// console.log(total);
+// let salaries = {}
+// salaries != new Object && console.log(0);
+
+// 4
+
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title : 'My menu'
+// }
+// multiplyNumeric(menu);
+// function multiplyNumeric(menu) {
+//     for (let key in menu) {
+//       if (typeof menu[key] == 'number') {
+//         menu[key] *= 2;
+//       }
+//     }
+// }
+// multiplyNumeric();
+// console.log(menu);
